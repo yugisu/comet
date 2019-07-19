b5199946b1151c0d120993293a219a0fa5028fbe
yugisu <slymahel@gmail.com>
2019-07-19 13:04:08 +0300

    Add logout button

--- src/client/src/components/app-header/index.tsx
+++ src/client/src/components/app-header/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
 import './style.scss';
+import { useSelector } from 'react-redux';
+import { StoreState } from '~store/types';
+import { User } from '~components/user';
 
 export function AppHeader() {
+  const currentUser = useSelector(({ user }: StoreState) => user.user);
+
+  const onUserClick = () => {
+    localStorage.removeItem('jwt');
+    location.reload();
+  };
+
   return (
     <div className='app-header'>
       <div className='app-header__logo'>
@@ -11,6 +21,16 @@ export function AppHeader() {
         </span>
         <span>comet</span>
       </div>
+      {currentUser && (
+        <div
+          className='app-header__user'
+          onClick={onUserClick}
+          role='button'
+          tabIndex={-1}
+        >
+          <User user={currentUser} />
+        </div>
+      )}
     </div>
   );
 }
